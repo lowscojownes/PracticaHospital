fe63f87fe729a0fe91e6a94ccc85f5961d84104e
mynesta <manuynesta@gmail.com>
2018-12-10 09:42:31 +0100

    hospital

--- PracticaHospital/src/practicahospital/Hospital.java
+++ PracticaHospital/src/practicahospital/Hospital.java
@@ -1,7 +1,28 @@
 package practicahospital;
 
-public class Hospital {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Hospital {
+    String nom;
+    Adreca adreca;
+    ArrayList<Historial> historials;
+    ArrayList<Malaltia> malaties;
+    private static Set<Pacient> pacients;
+    private static Set<Metge> metges;
+    
+
+    public Hospital(String nom,String ciutat,String codiPostal,String carrer,
+            int numero,String planta,String porta){
+        this.nom = nom;
+        adreca = new Adreca(ciutat,codiPostal,carrer,numero,planta,porta);
+        historials = new ArrayList<Historial>();
+        pacients = new HashSet<Pacient>();
+        malaties = new ArrayList<Malaltia>();
+        metges = new HashSet<Metge>();
+        
+    }
 
    
     
